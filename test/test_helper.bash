9dc025ba0434c4213981eecf04d192636b328bc8
Jaromir Hradilek <jhradilek@gmail.com>
2020-03-25 00:02:50 +0100

    Renamed and corrected the ContextInIDs rule.
    I also completely rewrote the tests to give myself better insight in
    what was tested.
    
    Known issues:
    
      * Due to the use of "scope: raw" to access AsciiDoc section ID
        definition, I am unable to ignore code blocks and block comments.
    
      * If there are two IDs with exactly the same string, one with and one
        without the {context} attribute, this rule incorrectly reports the
        line of the first one, even if it does have {context} defined.
        Considering how unlikely this is to happen in real situation, I
        don't think this is something I should worry about too much.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,14 +1,18 @@
 # Move to the directory with the corresponding sample files and run Vale
 # on them.
 #
-# Usage: run_vale BATS_TEST_FILENAME
+# Usage: run_vale BATS_TEST_FILENAME [SAMPLE_FILE]
 function run_vale {
   # Strip the directory name from the supplied file name:
-  local -r basename="${1##*/}"
+  local -r testname=${1##*/}
+
+  # Determine whether the sample file was specified and replace it with
+  # a dot if not:
+  local -r filename=${2:-'.'}
 
   # Compose the path to the directory with sample files:
-  local -r fixtures="fixtures/${basename/%.bats/}/"
+  local -r fixtures="fixtures/${testname/%.bats/}/"
 
   # Move the directory and run Vale on its contents:
-  cd "$fixtures" && \vale --output line --no-wrap --sort --config vale.ini .
+  cd "$fixtures" && \vale --output line --no-wrap --sort --config vale.ini $filename
 }
